053321aab123096d2c86ac20e5a3c9d251fd33e5
kthurman59 <kthurman59@gmail.com>
2024-09-09 12:37:20 -0500

    sensor model created

---
+++ backend/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, Float, String, DateTime
+from sqlalchemy.sql import func
+from .database import Base
+
+class SensorReading(Base):
+    tablename = "sensor_readings"
+
+    id = Column(Integer, primary_key=True, index=True)
+    sensor_id = Column(string, index=True)
+    temperature = Column(Float)
+    humidity = Column(Float)
+    soil_moisture = Column(Float)
+    timestamp = Column(DateTime(timezone=True), server_default=func.now())
+
+    
