28e3280fee4fe1d51618359b73527d654fcbf2af
kthurman59 <kthurman59@gmail.com>
2024-09-09 12:40:59 -0500

    sensor readings init

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
-from fastapi import FASTAPI;
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from . import models, database
+
+models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Welcome to the Precision Agriculture Platform API"};
+
+@app.get("/readings")
+def get_readings(db: Session = Depends(database.get_db)):
+    readings = db.query(models.SensorReading).all()
+    return readings
+    
